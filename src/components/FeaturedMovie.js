b58ee2523f5109ecb6b09d2537629bf50f6c389d
Gabriel Vieira <Gabrielvieirabarcelos02@gmail.com>
2020-11-12 17:18:23 -0300

    faltando header e footer pra finalizar home

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,6 +1,13 @@
 import './FeaturedMovie.css'
 
 function FeaturedMovie({item}){
+    console.log(item)
+    let firstDate = new Date(item.first_air_date);
+    let year  = firstDate.getFullYear()
+    let genres = []
+    for(let i in item.genres){
+        genres.push(item.genres[i].name)
+    }
     return(
         
         <section className="featured" style={{
@@ -14,7 +21,16 @@ function FeaturedMovie({item}){
                 <div className="featured--name">{item.original_name}</div>
                 <div className="featured--info">
                     <div className="featured--points">{item.vote_average} pontos</div>
+                    <div className="featured--year">{year}</div>
+                    <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons>1 ? 's' : ''}</div>
                 </div>
+                <div className="featured--description">{item.overview}</div>
+                <div className="featured--buttons">
+                    <a href={`/watch/${item.id}`} className="featured--watchbutton"> ▶ Assistir</a>
+                    <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+
+                </div>
+                 <div className="featured--genres"><strong>Gênero:</strong>{genres.join(', ')}</div>
                 </div>
             </div>
         </section>
